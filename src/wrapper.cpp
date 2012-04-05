df217112b1cc7a9142e01ef8d17cc530a69f06dd
Xavier Mendez <jmendeth@gmail.com>
2012-04-05 14:10:03 +0200

    Create base building and wrapper source

---
+++ src/wrapper.cpp
@@ -0,0 +1,42 @@
+/* ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This file is part of node-openni.
+ *
+ * node-openni is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * node-openni is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with node-openni.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * node-openni is Copyright (C) 2012, Xavier Mendez (jmendeth).
+ *
+ * ***** END GPL LICENSE BLOCK ***** */
+
+#include <v8.h>
+#include <node.h>
+
+using namespace v8;
+using namespace node;
+
+extern "C" {
+
+    static void init(Handle<Object> module) {
+        //Declare global objects (version, ...)
+        //TODO
+        
+        //Initialize every exposed class
+        //TODO
+        
+        printf("Init!\n");
+    }
+    
+    /** Finally, let Node.JS know about our module **/
+    NODE_MODULE(openni, init);
+}
